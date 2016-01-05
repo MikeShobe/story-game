247fb4ea23e896cc588f0e8d938eecb93d474490
MIKE SHOBE <mikeshobe@yahoo.com>
2016-01-04 19:23:31 -0800

    tweaked server file

--- server/server.js
+++ server/server.js
@@ -2,11 +2,10 @@ var express = require('express');
 var app = express();
 var browserify = require ('browserify');
 var bodyParser = require('body-parser');
-//var angular = require('angular');
-
-console.log("i am alive");
 
 app.use(express.static('client'));
+
+console.log("Server connected.");
 
 app.listen(3000);
 
